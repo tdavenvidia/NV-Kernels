4d9f9b8ec55d78378d29fa25be0faca67ac52acd
Ankit Agrawal <ankita@nvidia.com>
2025-11-27 17:06:27 +0000

    vfio: refactor vfio_pci_mmap_huge_fault function
    Refactor vfio_pci_mmap_huge_fault to take out the implementation
    to map the VMA to the PTE/PMD/PUD as a separate function.
    
    Export the new function to be used by nvgrace-gpu module.
    
    Move the alignment check code to verify that pfn and VMA VA is
    aligned to the page order to the header file and make it inline.
    
    No functional change is intended.
    
    Cc: Shameer Kolothum <skolothumtho@nvidia.com>
    Cc: Alex Williamson <alex@shazbot.org>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Reviewed-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>
    Link: https://lore.kernel.org/r/20251127170632.3477-2-ankita@nvidia.com
    Signed-off-by: Alex Williamson <alex@shazbot.org>
    (Backported from commit )
    [ tdave: minor conflcit in vfio_pci_core.h ]

--- include/linux/vfio_pci_core.h
+++ include/linux/vfio_pci_core.h
@@ -175,6 +175,9 @@ ssize_t vfio_pci_core_read(struct vfio_device *core_vdev, char __user *buf,
 		size_t count, loff_t *ppos);
 ssize_t vfio_pci_core_write(struct vfio_device *core_vdev, const char __user *buf,
 		size_t count, loff_t *ppos);
+vm_fault_t vfio_pci_vmf_insert_pfn(struct vfio_pci_core_device *vdev,
+				   struct vm_fault *vmf, unsigned long pfn,
+				   unsigned int order);
 int vfio_pci_core_mmap(struct vfio_device *core_vdev, struct vm_area_struct *vma);
 void vfio_pci_core_request(struct vfio_device *core_vdev, unsigned int count);
 int vfio_pci_core_match(struct vfio_device *core_vdev, char *buf);
@@ -217,6 +220,16 @@ VFIO_IOREAD_DECLARATION(32)
 #ifdef ioread64
 VFIO_IOREAD_DECLARATION(64)
 #endif
+
+static inline bool is_aligned_for_order(struct vm_area_struct *vma,
+					unsigned long addr,
+					unsigned long pfn,
+					unsigned int order)
+{
+	return !(order && (addr < vma->vm_start ||
+			   addr + (PAGE_SIZE << order) > vma->vm_end ||
+			   !IS_ALIGNED(pfn, 1 << order)));
+}
 
 int vfio_pci_dma_buf_iommufd_map(struct dma_buf_attachment *attachment,
 				 struct dma_buf_phys_vec *phys);
